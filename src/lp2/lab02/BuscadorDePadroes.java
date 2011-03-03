740127a7609c27851670dd73c58b749d9cd3c952
Jord?o Ezequiel <Jord?o Ezequiel@localhost>
2011-03-02 21:38:20 -0300

    new class

---
+++ src/lp2/lab02/BuscadorDePadroes.java
@@ -0,0 +1,32 @@
+package lp2.lab02;
+import java.util.Scanner;
+
+public class BuscadorDePadroes {
+
+	/**
+	 * Aluno: Jordao Ezequiel Serafim de Araujo
+	 * Matricula: 21021526
+	 */
+	public static void main(String[] args) {
+		String sequenciaRecebida = args[0], padrao;
+		Scanner input = new Scanner(System.in);
+		int numOcorrencias=0;
+		
+		sequenciaRecebida = sequenciaRecebida.toLowerCase();
+		sequenciaRecebida = sequenciaRecebida.replace(" ", "");
+		
+		System.out.print("Informe o padrão desejado: ");
+		padrao = input.next();
+		
+		padrao = padrao.toLowerCase();
+		padrao = padrao.replace(" ", "");
+		
+		for(int i=0; i<sequenciaRecebida.length(); i++)
+			if(sequenciaRecebida.contains(padrao)){
+				sequenciaRecebida = sequenciaRecebida.replaceFirst(padrao, padrao.toUpperCase());
+				numOcorrencias++;
+			}
+		if(numOcorrencias == 0) System.out.printf("ocorrências: %d\n", numOcorrencias);
+		else System.out.printf("ocorrências: %d - %s\n", numOcorrencias, sequenciaRecebida);
+	}
+}
