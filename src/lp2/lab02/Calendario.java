0b424cb6c516c0c8040d98c3c462baae04aa8d49
jordaotalk <jordaotalk@localhost>
2011-03-03 16:04:07 -0300

    metodo 3 completo. heheheh

--- src/lp2/lab02/Calendario.java
+++ src/lp2/lab02/Calendario.java
@@ -1,5 +1,6 @@
 package lp2.lab02;
 
+import java.io.ObjectInputStream.GetField;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Scanner;
@@ -23,7 +24,6 @@ public class Calendario {
 	/**
 	 * Este metodo define o laco principal do programa.
 	 */
-	
 	private static void lacoMain(){
 		int opcao=4;
 		do{
@@ -70,26 +70,23 @@ public class Calendario {
 	 * Metodo responsavel por encontrar o dia da semana atraves da data passada pelo usuario.
 	 */
 	private static void indicaDiaSemana() {
-		Calendar calendario = new GregorianCalendar();
-		final String SEMANA[] = {"Domingo", "Segunda Feira","Terca Feira","Quarta Feira","Quinta Feira","Sexta Feira","Sabado"};
-		final int CONSTANTE = 1;
+		GregorianCalendar calendario = new GregorianCalendar();
+		final String SEMANA[] = {"", "Domingo", "Segunda Feira","Terca Feira","Quarta Feira","Quinta Feira","Sexta Feira","Sabado"};
+		final int CONSTANTE_CORRECAO = 1;
 		int dia, mes, ano;
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("\nInsira o dia [1 a 31]: ");
 		dia = input.nextInt();
-		System.out.print("Insira o mes [1=Janeiro, ..., 12=Dezembro]: ");
+		System.out.print("Insira o mes [1 a 12]: ");
 		mes = input.nextInt();
-		System.out.print("Insira o ano [Ano desejado]: ");
+		System.out.print("Insira o ano [ano desejado]: ");
 		ano = input.nextInt();
 		
-		dia -= CONSTANTE;
-		mes -= CONSTANTE;
-		
 		calendario.clear();
-		calendario.set(ano, mes, dia);
+		calendario.set(ano, (mes - CONSTANTE_CORRECAO), dia);
 		
-		System.out.printf("\nDia %d/%d/%d eh um(a): %s\n\n", dia+CONSTANTE, mes+CONSTANTE, ano, SEMANA[calendario.get(Calendar.DAY_OF_WEEK)]);	
+		System.out.printf("\nDia %d/%d/%d eh um(a): %s\n\n", dia, mes, ano, SEMANA[calendario.get(Calendar.DAY_OF_WEEK)]);	
 	}
 	
 	/**
@@ -114,31 +111,40 @@ public class Calendario {
 	 */
 	private static void diasUteisMes(){
 		
-		Calendar calendario = new GregorianCalendar();
-		GregorianCalendar calendar = new GregorianCalendar();
-		final int CONSTANTE = 1;
-		int mes, ano, diasUteis=0;
+		GregorianCalendar calendario = new GregorianCalendar();
+		final int CONSTANTE_CORRECAO = 1, DIA = 1;
+		int mes, ano, diasUteis = 0, qtdDias = 0;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("\nInsira o mes [1=Janeiro, ..., 12=Dezembro]: ");
+		System.out.print("\nInsira o mes [1 a 12]: ");
 		mes = input.nextInt();
-		System.out.print("Insira o ano [Ano desejado]: ");
+		System.out.print("Insira o ano [ano desejado]: ");
 		ano = input.nextInt();
 		
-		mes -= CONSTANTE;
+		mes -= CONSTANTE_CORRECAO;
 		
-		calendario.set(ano, mes, 0);
+		calendario.set(ano, mes, DIA);
 		
-		if(calendar.isLeapYear(ano)){
-			if(mes+CONSTANTE == 2){
-				for(int i=1; i<=28; i++){
-					if(calendar.get(Calendar.DAY_OF_WEEK) != 0 && calendar.get(Calendar.DAY_OF_WEEK) != 6){
-						diasUteis++;
-					}
-				}
+		if(mes == 0 || mes == 2 || mes == 4 || mes == 6 || mes == 7 || mes == 9 || mes == 11){
+			qtdDias = 31;
+		} else if(mes == 3 || mes == 5 || mes == 8 || mes == 10){
+			qtdDias = 31;
+		} else if(mes == 1){
+			if(calendario.isLeapYear(ano)){
+				qtdDias = 29;
+			} else{
+				qtdDias = 28;
 			}
 		}
-		System.out.println(diasUteis);
+		
+		for(int i = DIA; i <= qtdDias; i++){
+			calendario.set(ano, mes, i);
+			if(calendario.get(GregorianCalendar.DAY_OF_WEEK) != GregorianCalendar.SUNDAY && calendario.get(GregorianCalendar.DAY_OF_WEEK) != GregorianCalendar.SATURDAY){
+				diasUteis++;
+			}
+		}
+		
+		System.out.println("dias uteis: "+diasUteis);
 		
 	}
 
