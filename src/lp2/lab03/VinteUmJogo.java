cf99d2f9cd79312d2eab99d5fe0665020a7b3299
jordaotalk <jordaotalk@localhost>
2011-03-10 10:43:28 -0300

    modificacoes: tirei um while

--- src/lp2/lab03/VinteUmJogo.java
+++ src/lp2/lab03/VinteUmJogo.java
@@ -22,7 +22,10 @@ public class VinteUmJogo {
 	}
 	static boolean verificaJogador = true;
 	static boolean verificaMesa = true;
-
+	
+	/**
+	 * Metodo que mostra o menu principal do jogo.
+	 */
 	private static void menu(){
 		verificaJogador = true;
 		verificaMesa = true;
@@ -42,6 +45,9 @@ public class VinteUmJogo {
 		
 	}
 	
+	/**
+	 * Metodo que inicia um novo jogo. Aqui eh criado um novo baralho.
+	 */
 	private static void startGame() {
 		
 		Baralho baralho = new Baralho();
@@ -49,27 +55,32 @@ public class VinteUmJogo {
 		jogar(baralho);
 	}
 	
+	/**
+	 * Metodo que pergunta ao usuario se ele deseja continuar jogando.
+	 * @param baralho Este baralho foi criado no metodo startGame().
+	 */
 	private static void continuarJogando(Baralho baralho) {
-		if(baralho.númeroDeCartas() > 0){
-			String opcao;
-			System.out.println("\n# Deseja continuar Jogando? #");
-			System.out.println("# 1 - Sim                   #");
-			System.out.println("# outra opcao - Nao         #");
-			System.out.println("#############################");
-			System.out.print(">>> ");
-			opcao = (new Scanner(System.in)).nextLine();
-			if(opcao.equals("1")){
-				verificaJogador = true;
-				verificaMesa = true;
-				while(true){
-					jogar(baralho);
-				}
-			}else{
-				System.out.println("\n");
-			}
+		String opcao;
+		System.out.println("\n# Deseja continuar Jogando? #");
+		System.out.println("# 1 - Sim                   #");
+		System.out.println("# outra opcao - Nao         #");
+		System.out.println("#############################");
+		System.out.print(">>> ");
+		opcao = (new Scanner(System.in)).nextLine();
+		if(opcao.equals("1")){
+			verificaJogador = true;
+			verificaMesa = true;
+			jogar(baralho);
+		}else{
+			System.out.println("\n");
 		}
 	}
 
+	/**
+	 * Metodo que adiciona cartas a mao do jogador.
+	 * @param mao Esta eh a mao do jogador que eh passada como parametro para este metodo adicionar cartas nela.
+	 * @param baralho Este eh o baralho que eh utilizado para dar cartas tanto a mesa quanto ao jogador.
+	 */
 	private static void adicionarCartasJogador(Mao mao, Baralho baralho){
 		String opcao;
 		System.out.println("# Deseja pegar uma carta? ####");
@@ -80,7 +91,7 @@ public class VinteUmJogo {
 		System.out.print(">>> ");
 		opcao = (new Scanner(System.in)).nextLine();
 		if(opcao.equals("1")){
-			if(baralho.númeroDeCartas() > 0){
+			if(baralho.nÃºmeroDeCartas() > 0){
 				mao.adicionar(baralho.pegaCarta());
 			}else{
 				System.out.println("Nao ha mais cartas no baralho.");
@@ -95,8 +106,13 @@ public class VinteUmJogo {
 		}
 	}
 	
+	/**
+	 * Metodo que adiciona cartas a mao da mesa.
+	 * @param mao Esta eh a mao de cartas da mesa.
+	 * @param baralho Este eh o baralho que eh utilizado para dar cartas tanto a mesa quanto ao jogador.
+	 */
 	private static void adicionarCartasMesa(Mao mao, Baralho baralho){
-		if (baralho.númeroDeCartas() > 0){
+		if (baralho.nÃºmeroDeCartas() > 0){
 			if (mao.valor() < 17){
 				mao.adicionar(baralho.pegaCarta());
 			}else{
@@ -110,6 +126,10 @@ public class VinteUmJogo {
 		}
 	}
 
+	/**
+	 * Metodo que contem todos os comandos necessarios para executar o jogo.
+	 * @param baralho Este eh o baralho do jogo.
+	 */
 	private static void jogar(Baralho baralho){
 		Mao maoJogador = new Mao();
 		Mao maoMesa = new Mao();
@@ -123,7 +143,7 @@ public class VinteUmJogo {
 			
 			System.out.printf("Mao do Jogador: %s\n", maoJogador.toString());
 			System.out.printf("Pontos do Jogador: %d\n", maoJogador.valor());
-			System.out.printf("Quantidade de cartas da Mesa: %s\n\n", maoMesa.númeroDeCartas());
+			System.out.printf("Quantidade de cartas da Mesa: %s\n\n", maoMesa.nÃºmeroDeCartas());
 			
 			if(verificaJogador){
 				adicionarCartasJogador(maoJogador, baralho);
@@ -142,6 +162,8 @@ public class VinteUmJogo {
 					System.out.println("\n##Situacao do Jogo: Mao do Jogador Estourou.##\n");
 				}else if(maoMesa.valor() == 21){
 					System.out.println("\n##Situacao do Jogo: Mesa atingiu 21 pontos.##\n");
+				}else if(maoMesa.valor() == maoJogador.valor()){
+					System.out.println("\n##Situacao do Jogo: EMPATE.##\n");
 				}
 				break;
 			}else if(maoMesa.valor() > 21 || maoJogador.valor() == 21){
@@ -154,17 +176,21 @@ public class VinteUmJogo {
 					System.out.println("\n##Situacao do Jogo: Mao da Mesa Estourou.##\n");
 				}else if(maoJogador.valor() == 21){
 					System.out.println("\n##Situacao do Jogo: Jogador atingiu 21 pontos.##\n");
+				}else if(maoMesa.valor() == maoJogador.valor()){
+					System.out.println("\n##Situacao do Jogo: EMPATE.##\n");
 				}
 				break;
 			}
 			
 			if(!verificaJogador && !verificaMesa){
+				
 				if(maoJogador.valor() > maoMesa.valor()){
 					System.out.println("\n### Jogador Ganhou!!!!!!! ###\n");
 					System.out.printf("Mao do Jogador: %s\n", maoJogador.toString());
 					System.out.printf("Pontos do Jogador: %d\n", maoJogador.valor());
 					System.out.printf("Mao da Mesa: %s\n", maoMesa.toString());
 					System.out.printf("Pontos da Mesa: %d\n\n", maoMesa.valor());
+					System.out.println("##Situacao do Jogo: Jogador tem mais pontos que a Mesa.##\n");
 					break;
 				}else if(maoMesa.valor() > maoJogador.valor() || maoJogador.valor() == maoMesa.valor()){
 					System.out.println("\n### Mesa Ganhou!!!!! ###\n");
@@ -172,12 +198,13 @@ public class VinteUmJogo {
 					System.out.printf("Pontos do Jogador: %d\n", maoJogador.valor());
 					System.out.printf("Mao da Mesa: %s\n", maoMesa.toString());
 					System.out.printf("Pontos da Mesa: %d\n\n", maoMesa.valor());
+					System.out.println("##Situacao do Jogo: Mesa tem mais pontos ou mesma quantidade de pontos do Jogador.##\n");
 					break;
 				}
 			}
 		}
-		System.out.printf("QUANTIDADE DE CARTAS NO BARALHO: %d\n", baralho.númeroDeCartas());
-		if(baralho.númeroDeCartas() >= 2){
+		System.out.printf("QUANTIDADE DE CARTAS NO BARALHO: %d\n", baralho.nÃºmeroDeCartas());
+		if(baralho.nÃºmeroDeCartas() >= 2){
 			continuarJogando(baralho);
 		}else{
 			System.out.println("\n### QUANTIDADE DE CARTAS INSUFICIENTE PARA JOGAR. ###\n");
--- src/lp2/lab03/VinteUmPreparacao.java
+++ src/lp2/lab03/VinteUmPreparacao.java
@@ -24,11 +24,11 @@ public class VinteUmPreparacao {
 		final int NUMERO_MININO = 1;
 		do{
 			System.out.print("\nSua mao tem esta(s) carta(s): " + maoJogador);
-			System.out.printf("\nSua mao tem %d cartas.\nDigite algo para pegar uma carta: ", maoJogador.númeroDeCartas());
+			System.out.printf("\nSua mao tem %d cartas.\nDigite algo para pegar uma carta: ", maoJogador.nÃºmeroDeCartas());
 			opcao = (new Scanner(System.in)).nextLine();
-			if(baralho.númeroDeCartas() >= NUMERO_MININO){
+			if(baralho.nÃºmeroDeCartas() >= NUMERO_MININO){
 				maoJogador.adicionar(baralho.pegaCarta());
-			}else if(baralho.númeroDeCartas() == 0){
+			}else if(baralho.nÃºmeroDeCartas() == 0){
 				System.out.println("O BARALHO ACABOU.");
 			}
 		}while (!opcao.equals("t"));
