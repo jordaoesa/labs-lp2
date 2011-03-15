f17360b9e9562a9a51ea471bf691c1b45fa414e9
jordaotalk <jordaotalk@localhost>
2011-03-15 15:02:56 -0300

    modificacoes

--- src/lp2/lab03/VinteUmPreparacao.java
+++ src/lp2/lab03/VinteUmPreparacao.java
@@ -24,11 +24,11 @@ public class VinteUmPreparacao {
 		final int NUMERO_MININO = 1;
 		do{
 			System.out.print("\nSua mao tem esta(s) carta(s): " + maoJogador);
-			System.out.printf("\nSua mao tem %d cartas.\nDigite algo para pegar uma carta: ", maoJogador.nÃºmeroDeCartas());
+			System.out.printf("\nSua mao tem %d cartas.\nDigite algo para pegar uma carta: ", maoJogador.númeroDeCartas());
 			opcao = (new Scanner(System.in)).nextLine();
-			if(baralho.nÃºmeroDeCartas() >= NUMERO_MININO){
+			if(baralho.númeroDeCartas() >= NUMERO_MININO){
 				maoJogador.adicionar(baralho.pegaCarta());
-			}else if(baralho.nÃºmeroDeCartas() == 0){
+			}else if(baralho.númeroDeCartas() == 0){
 				System.out.println("O BARALHO ACABOU.");
 			}
 		}while (!opcao.equals("t"));
