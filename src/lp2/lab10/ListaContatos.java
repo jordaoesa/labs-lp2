70b00faec81f6161513e08a74744ff3caecc97a2
Jordaotalk <Jordaotalk@localhost>
2011-04-28 08:55:41 -0300

    listaContatos

--- src/lp2/lab10/ListaContatos.java
+++ src/lp2/lab10/ListaContatos.java
@@ -1,10 +1,6 @@
 package lp2.lab10;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class ListaContatos {
 	
-	private List<ItemDeContato> listaContatos = new ArrayList<ItemDeContato>();
 
 }
